2479c33fc760a115250db416f51c739b1007c34d
ExBook <weijian.me@gmail.com>
2026-05-24 19:26:31 +0800

    feat(p1): add Dexie database schema

---
+++ src/repo/db.ts
@@ -0,0 +1,21 @@
+import Dexie, { type Table } from 'dexie';
+import type { Bank, Note, Question, QuizRecord } from '../shared/types';
+
+export class ExLocalDB extends Dexie {
+  banks!: Table<Bank, string>;
+  questions!: Table<Question, string>;
+  quizRecords!: Table<QuizRecord, string>;
+  notes!: Table<Note, string>;
+
+  constructor() {
+    super('exlocal');
+    this.version(1).stores({
+      banks: 'id, updatedAt',
+      questions: 'id, bankId, order',
+      quizRecords: 'id, questionId, bankId, timestamp',
+      notes: 'id, questionId, bankId',
+    });
+  }
+}
+
+export const db = new ExLocalDB();
